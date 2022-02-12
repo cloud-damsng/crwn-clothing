f567f5df6c5263227a1983becf985d2295acb439
damsng <damsng@gmail.com>
2022-02-12 08:25:10 +0100

    home page directory page moved to redux

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -4,6 +4,7 @@ import storage from 'redux-persist/lib/storage';
 
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer';
+import directoryReducer from './directory/directoryReducer';
 
 const persistConfig ={
     key: 'root',
@@ -13,7 +14,8 @@ const persistConfig ={
 
 const rootReducer = combineReducers({
     user: userReducer,
-    cart: cartReducer
+    cart: cartReducer,
+    directoryReducer: directory
 });
 
 export default persistReducer(persistConfig, rootReducer);
